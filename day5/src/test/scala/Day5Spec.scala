6ee2ad07de90046be808735d88de4b947fd97a29
Alexander Kopfinger <alexander@kopfinger.se>
2021-12-05 10:33:04 +0100

    Day 5 part 2

--- day5/src/test/scala/Day5Spec.scala
+++ day5/src/test/scala/Day5Spec.scala
@@ -1,11 +1,8 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-
 import scalaj.http.Http
 
 import scala.util.chaining.scalaUtilChainingOps
-import scala.annotation.tailrec
-import java.lang.Math.{abs, max}
 
 class Day5Spec extends AnyFlatSpec with Matchers {
   extension [A, B](a: A) def |(f: (A) => B): B = a.pipe(f)
@@ -26,19 +23,29 @@ class Day5Spec extends AnyFlatSpec with Matchers {
     val reg = "(.*),(.*) -> (.*),(.*)".r
 
     case class Line(x1: Int, x2: Int, y1: Int, y2: Int) {
-      def stepDir = (if isHorizontal then (y1, y2)
-                     else (x1, x2)).pipe((start, end) => if (start > end) then -1 else 1)
+      def stepDir(start: Int, end: Int): Int = if (start > end) then -1 else 1
+
       def getCoordinates: Vector[(Int, Int)] =
-        (if isHorizontal then for (i <- y1 to y2 by stepDir) yield (x1, i)
-         else for (i <- x1 to x2 by stepDir) yield (i, y1)).toVector
+        if isDiagonal then getDiagonalCoordinates
+        else
+          (if isHorizontal then for (i <- y1 to y2 by stepDir(y1, y2)) yield (x1, i)
+           else for (i <- x1 to x2 by stepDir(x1, x2)) yield (i, y1)).toVector
+
+      def getDiagonalCoordinates: Vector[(Int, Int)] = {
+        val xs = for (x <- x1 to x2 by stepDir(x1, x2)) yield x
+        val ys = for (y <- y1 to y2 by stepDir(y1, y2)) yield y
+
+        xs.zipWithIndex.map(((x, i) => (x, ys(i)))).toVector
+      }
 
       def isHorizontal: Boolean = x1 == x2
       def isVertical: Boolean = y1 == y2
+      def isDiagonal: Boolean = !isHorizontal && !isVertical
     }
 
-    def countOverlapping(lines: Vector[Line]): Int = {
+    def countOverlapping(lines: Vector[Line], includeDiagonal: Boolean = false): Int = {
       lines
-        .filter(r => r.isHorizontal || r.isVertical)
+        .filter(r => includeDiagonal || (r.isHorizontal || r.isVertical))
         .flatMap(_.getCoordinates)
         .groupMapReduce(identity)(_ => 1)(_ + _)
         .count { case (_, n) => n > 1 }
@@ -51,5 +58,14 @@ class Day5Spec extends AnyFlatSpec with Matchers {
       Line(x1.toInt, x2.toInt, y1.toInt, y2.toInt)
     }
     countOverlapping(lines) shouldBe 7142
+  }
+
+  "part-2" should "include countDiagonals" in new SubmarineSeismologyFunctions {
+    val input = aocInput | trimValues | toVector
+    val lines = input.collect { case reg(x1, y1, x2, y2) =>
+      Line(x1.toInt, x2.toInt, y1.toInt, y2.toInt)
+    }
+
+    countOverlapping(lines, includeDiagonal = true) shouldBe 2
   }
 }
